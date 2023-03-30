5b6520863eb11338b456b6d1ed6fd2d1c1b9bcf0
Emmanuel Giron <emmus.giron@gmail.com>
2023-03-30 10:01:34 -0600

    pago tarjeta

--- View/menu.php
+++ View/menu.php
@@ -4,7 +4,7 @@
         <p>Siempre los mejores estilos</p>
         <ul class="nav navbar-nav navbar-right">
         <li class="">
-              <a href="#" class="" > Carrito(<?php
+              <a href="<?=PATH.'/Productos/mostrarCarrito/'?>" class="" > Carrito(<?php
                         //condicionador ternario
                         echo (empty($_SESSION['CARRITO'])?0:array_sum(array_column($_SESSION['CARRITO'],"CANTIDAD")));
                     ?>)</a>
