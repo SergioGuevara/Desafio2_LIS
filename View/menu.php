caec1d142f771b92a350632e14bbf4b87f35ba4f
SergioGuevara <49651732+SergioGuevara@users.noreply.github.com>
2023-03-30 11:45:13 -0600

    cambios

--- View/menu.php
+++ View/menu.php
@@ -1,27 +1,17 @@
-<nav class="navbar navbar-dark bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar w/ text</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-      </ul>
-      <span class="navbar-text">
-        Navbar text with an inline element
-      </span>
-    </div>
-  </div>
-</nav>
+<header class="head">
+    
+        <h1 class="title">TextilExport</h1>
+        <p>Siempre los mejores estilos</p>
+        <ul class="nav navbar-nav navbar-right">
+        <li class="">
+              <a href="<?=PATH.'/Productos/mostrarCarrito/'?>" class="" > Carrito(<?php
+                        //condicionador ternario
+                        echo (empty($_SESSION['CARRITO'])?0:array_sum(array_column($_SESSION['CARRITO'],"CANTIDAD")));
+                    ?>)</a>
+            </li>
+        </ul>
+    </header>
+        
 
 
 
